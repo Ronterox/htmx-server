0a572d65bc988f6f465fd7243cb5eac31f641e8e
Ricardo <ricarditomontserrat@gmail.com>
2024-06-03 19:41:31 +0200

    Better cleaned version

--- count.php
+++ count.php
@@ -4,6 +4,8 @@ include 'view.php';
 
 session_start();
 
-$_SESSION['counter'] = $_SESSION['counter'] + 1;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_SESSION['counter'] = $_SESSION['counter'] + 1;
+}
 
 view('index.html', 'count', ['count' => $_SESSION['counter']]);
