2bdb1ebf3866726ee1a01e9c1073ea939cf965cb
Ricardo <ricarditomontserrat@gmail.com>
2024-06-06 01:31:57 +0200

    Secured filtering, and deleting of contacts

--- contact.php
+++ contact.php
@@ -5,20 +5,30 @@ include 'view.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $contact = compact('name', 'email');
+    $id = ++$_SESSION['id'];
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $contact = compact('id', 'name', 'email');
 
     if (!isset($_SESSION['contacts'])) {
         $_SESSION['contacts'] = [];
     }
 
-    array_push($_SESSION['contacts'], $contact);
+    $_SESSION['contacts'][$id] = $contact;
     view('index.html', 'oob-contact', $contact);
+
 } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    if ($_REQUEST['id'] === 'all') {
+    if ($_GET['id'] === 'all') {
         session_unset();
         session_destroy();
         view('index.html', 'all');
+    } elseif (isset($_GET['id'])) {
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+        if (isset($id) && isset($_SESSION['contacts'][$id])) {
+            unset($_SESSION['contacts'][$id]);
+        } else {
+            http_response_code(404);
+        }
     }
 }
--- index.php
+++ index.php
@@ -5,12 +5,14 @@ include 'view.php';
 session_start();
 
 $contact = [
+    'id' => 1,
     'name' => 'John Doe',
     'email' => 'jd@email.com'
 ];
 
-$contacts = $_SESSION['contacts'] ?? [$contact];
+$contacts = $_SESSION['contacts'] ?? [$contact['id'] => $contact];
 
 view('index.html', 'all', compact('contacts'));
 
 $_SESSION['contacts'] = $contacts;
+$_SESSION['id'] ??= $contact['id'];
